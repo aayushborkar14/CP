f20dd957f8ba44001110d4c63f17ae40f4d2adf0
AbstractGhoul05 <aayushb14@gmail.com>
2021-01-03 22:57:04 +0530

    POJ

--- POJ/assn2/1654.cpp
+++ POJ/assn2/1654.cpp
@@ -14,7 +14,6 @@ int main() {
         long long int prev_x = 0;
         long long int prev_y = 0;
         string my_str;
-        long long int term1{ }, term2{ };
         cin.ignore();
         getline(cin, my_str);
         long double area{ };
@@ -28,20 +27,20 @@ int main() {
             } else if (temp == '2') {
                 current_y--;
             } else if (temp == '6') {
-                current_x++;
-            } else if (temp == '4') {
                 current_x--;
-            } else if (temp == '9') {
+            } else if (temp == '4') {
                 current_x++;
+            } else if (temp == '9') {
+                current_x--;
                 current_y++;
             } else if (temp == '7') {
-                current_x--;
+                current_x++;
                 current_y++;
             } else if (temp == '3') {
-                current_x++;
+                current_x--;
                 current_y--;
             } else if (temp == '1') {
-                current_x--;
+                current_x++;
                 current_y--;
             }
             area += current_x*prev_y - prev_x*current_y;
--- POJ/assn2/2693.cpp
+++ POJ/assn2/2693.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    vector<pair<float, float>> vec;
+    float x{}, y{};
+    while(!cin.eof()) {
+        cin >> x >> y;
+        vec.push_back(make_pair(x, y));
+    }
+    for (auto pair_temp : vec) {
+        cout << pair_temp.first << " " << pair_temp.second << "\n";
+    }
 }
